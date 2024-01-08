50a160941500eeb4baed8ac0e00ea4a2d18b548a
dearmannawar <dearmannawar@gmail.com>
2024-01-08 18:18:21 +0300

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,50 +1,18 @@
-import Task from './Task';
+import Text from './Text';
 import { useState } from 'react';
 import './App.css';
 
 function App() {
-  const [todoList, setToDoList] = useState([]);
-  const [newTask, setNewTask] = useState("");
-
-  const handleChange =(event) => {
-    setNewTask(event.target.value);
-  }
-
-  const addTask = () => {
-    const task = {
-      id: todoList.length === 0? 1: todoList[todoList.length -1].id +1,
-      taskName: newTask,
-      completed: false
-    };
-    setToDoList([...todoList, task]);
-  }
-
-  const deleteTask = (id) => {
-    setToDoList(todoList.filter((task) => task.id !== id));
-  }
-
-  const completeTask = (id) => {
-    setToDoList(todoList.map((task) => {
-      if(task.id === id){
-          return {...task, completed: !task.completed}
-      }
-      return task;
-    }))
-  }
+  const [showText, setShowText] = useState(false);
 
   return (
     <>
       <div className='App'>
         "Allah is the only God!"
-        <div className='addTask'>
-          <input onChange={handleChange}/>
-          <button onClick={addTask}>Add Task</button>
-        </div>
-        <div className='list'>
-          {todoList.map((task) => {
-            return <Task key={task.id} taskName={task.taskName} id={task.id} completed={task.completed} completeTask={completeTask} deleteTask={deleteTask}/>
-          })}
-        </div>
+        <button onClick={() => {
+          setShowText(!showText)
+        }}>Show Text</button>
+        {showText && <Text/>}
       </div>
     </>
   );
---
+++ src/Text.js
@@ -0,0 +1,23 @@
+import { useState, useEffect } from "react"
+
+const Text = () => {
+    const [text, setText] = useState("");
+    useEffect(() => {
+        console.log("Component mounted");
+        return(() => {
+            console.log("Component unmounted")
+        })
+    },[text])
+
+    return(
+        <div>
+            <input onChange={(event) => {
+                setText(event.target.value)
+            }}
+            />
+            <h1>{text}</h1>
+        </div>
+    )
+}
+
+export default Text;
