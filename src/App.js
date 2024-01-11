39ae513e91f349d1f5f91a5e40de1efa1533e240
dearmannawar <dearmannawar@gmail.com>
2024-01-11 19:09:08 +0300

    custom hooks

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import './App.css';
-
-import { Form } from './pages/Form.js';
+import { useToggle } from './useToggle';
 
 function App() {
+  const[isVisible, toggle] = useToggle();
+
 
   return (
     <>
       <div className='App'>
-
         لا إله إلا الله محمد رسول الله
-        <Form />
- 
+        <button onClick={() => toggle() }>
+          {isVisible ? "Hide": "Show"}
+        </button>
+        {isVisible && <h1>Hidden Text</h1>}
+
       </div>
     </>
   );
